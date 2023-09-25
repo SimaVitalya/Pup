6be2157f47c8dbcdd10440f11f750224548f918e
SimaVitalay <simachev28@gmail.com>
2023-09-25 12:49:34 +0300

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 // const port = 3000;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(cors()); // Enable CORS for all routes
 
 app.get('/car/:vin', async (req, res) => {
